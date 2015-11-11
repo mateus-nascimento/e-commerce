296cc23bf61528928de4254aab88890ef8231dff
bboyrap <bboyrap@mateus-PC>
2015-11-11 04:41:53 -0300

    Falta coisa.

--- src/view/desktop/usuario/FormUsuario.java
+++ src/view/desktop/usuario/FormUsuario.java
@@ -25,6 +25,7 @@ public class FormUsuario extends javax.swing.JInternalFrame {
     public FormUsuario() {
         initComponents();
         buscarUsuarios();
+        JOptionPane.showMessageDialog(null, "Teoricamente já entrou no formUsuario e listou tds usuarios;");
     }
     
     private List<Usuario> lista;
@@ -79,19 +80,20 @@ public class FormUsuario extends javax.swing.JInternalFrame {
         jButtonSair = new javax.swing.JButton();
 
         setTitle("Usuário");
+        setPreferredSize(new java.awt.Dimension(1200, 650));
         setVisible(true);
 
         jPanel2.setBorder(javax.swing.BorderFactory.createTitledBorder("Usuários"));
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null, null},
-                {null, null, null, null, null},
-                {null, null, null, null, null},
-                {null, null, null, null, null}
+                {},
+                {},
+                {},
+                {}
             },
             new String [] {
-                "id", "nome", "email", "cpf", "status"
+
             }
         ));
         jScrollPane1.setViewportView(jTable1);
@@ -101,7 +103,7 @@ public class FormUsuario extends javax.swing.JInternalFrame {
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel2Layout.createSequentialGroup()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 1173, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 1126, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(0, 0, Short.MAX_VALUE))
         );
         jPanel2Layout.setVerticalGroup(
@@ -220,15 +222,16 @@ public class FormUsuario extends javax.swing.JInternalFrame {
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addContainerGap())
+                .addContainerGap()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
 
-        setLocation(new java.awt.Point(0, 0));
+        setBounds(0, 0, 1200, 650);
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButtonInserirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonInserirActionPerformed
